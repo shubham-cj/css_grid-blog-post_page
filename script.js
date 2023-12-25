359685756e052058ec06ac19dc84c20b707adb03
shubham-cj <119930498+shubham-cj@users.noreply.github.com>
2023-12-25 22:04:02 +0530

    Javascript added

---
+++ script.js
@@ -0,0 +1,72 @@
+let articleHeading = document.getElementById("articleHeading");
+let articleImg = document.getElementById("articleImg");
+let articleDescription = document.getElementById("articleDescription");
+let date = document.getElementById("date");
+let category = document.getElementById("category");
+let author = document.getElementById("author");
+
+function articleFilm(){
+    articleHeading.textContent = "Most successful film directors"
+    articleImg.src = "https://images.unsplash.com/photo-1485846234645-a62644f84728?q=80&w=1718&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    articleImg.alt = "take image"
+    articleDescription.innerHTML = 
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam sint atque tempora voluptatum culpa voluptatem dignissimos! Quisquam sit numquam ad perspiciatis! Earum aspernatur a aliquam debitis, sunt exercitationem illo alias? <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam quia iusto reiciendis. Fugit iste eligendi cupiditate unde, similique dolorum facilis molestias rerum nemo, quam quas cum. Possimus corporis repellendus sequi porro molestiae, nostrum suscipit odit eos ducimus mollitia quo placeat. <br><br> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat molestiae repellendus magni maiores ad, nam aperiam sint mollitia doloremque tempore, quia enim natus fuga autem amet deserunt pariatur ullam ratione. Nam reiciendis perspiciatis, quod reprehenderit ex quisquam eligendi aut optio aliquam sunt voluptatibus repudiandae ea unde, minima voluptatum incidunt? Cumque."
+    date.textContent = "Dec 2nd 2004"
+    category.textContent = "Film & Cinema"
+    author.textContent = "Lusi"
+}
+
+function articleBook(){
+    articleHeading.textContent = "Most popular Books in 2023"
+    articleImg.src = "https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?q=80&w=1674&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    articleImg.alt = "Book image"
+    articleDescription.innerHTML = 
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, veritatis? <br><br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nisi similique quasi fuga adipisci impedit ex tenetur sit incidunt repellendus quaerat eveniet, soluta fugiat ad iusto totam dicta maiores ea odio esse et eaque atque dolores corrupti. Unde illo rem possimus aliquid dolore beatae consequatur, excepturi porro quam, facere autem et soluta quos voluptatibus maiores perspiciatis, harum tempora voluptates. Ratione commodi at repudiandae molestias ullam exercitationem nemo iure tenetur nihil. <br><br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur magni, alias quis voluptatum in ad ipsam ab asperiores esse expedita? <br><br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, cupiditate."
+    date.textContent = "Jan 5th 2010"
+    category.textContent = "Book & Reading"
+    author.textContent = "Tom" 
+}
+
+function articleGame(){
+    articleHeading.textContent = "The best 5 Battelground Games"
+    articleImg.src = "https://images.unsplash.com/photo-1564049489314-60d154ff107d?q=80&w=1783&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    articleImg.alt = "article banner"
+    articleDescription.innerHTML = 
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci molestiae ratione in neque aut, a laudantium, dolor rerum atque quam alias. Ut cum iure quos dolore obcaecati explicabo nulla id eligendi labore eveniet blanditiis impedit facilis perspiciatis hic, exercitationem sed officia dolor sunt sint vitae? Incidunt, expedita accusantium, necessitatibus ex, modi voluptas impedit recusandae fuga aliquid quisquam labore perspiciatis similique. <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque recusandae quibusdam voluptatum incidunt aspernatur ullam maxime delectus sunt perspiciatis omnis! <br> <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta harum, incidunt ducimus omnis similique a ut voluptatum quam. Consequatur fugiat doloribus saepe eum magnam itaque neque recusandae? Pariatur, sit non voluptatibus laborum consequuntur, porro dolorum, in dicta quidem magnam alias."
+    date.textContent = "Sep 8th 1999"
+    category.textContent = "Games & Consoles"
+    author.textContent = "Lorem"
+}
+
+function articleFood(){
+    articleHeading.textContent = "Food vloggers have now reached every street's in India"
+    articleImg.src = "https://images.unsplash.com/photo-1543393786-6b9844cc148d?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    articleImg.alt = "food"
+    articleDescription.innerHTML = 
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsum harum hic exercitationem aut ratione culpa perferendis sint porro, numquam distinctio officiis veritatis quam voluptates, quae dicta, sunt accusantium rerum? At suscipit quo non corporis dolores nemo provident numquam aut recusandae, cupiditate facilis perferendis. Asperiores unde assumenda voluptatem esse repellat animi quas iste obcaecati quo ducimus modi, corporis id nemo. Iure libero magnam doloribus qui est nulla impedit recusandae laboriosam. <br><br> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo explicabo voluptas facere sed, soluta impedit nemo consequatur rerum dicta vel perspiciatis, exercitationem nihil. Velit quasi minima iure impedit, doloribus accusamus. <br><br> Lorem ipsum dolor sit amet."
+    date.textContent = "Jun 29th 2014" 
+    category.textContent = "Food & Drink"
+    author.textContent = "Jennifer"
+}
+
+function articleWorld(){
+    articleHeading.textContent = "News channels in all over the world"
+    articleImg.src = "https://images.unsplash.com/photo-1581092787765-e3feb951d987?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    articleImg.alt = "News Studio"
+    articleDescription.innerHTML = 
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae inventore modi quam reprehenderit eaque, magnam consectetur sapiente in, rerum repellat dicta iste esse quis nam, odit veniam adipisci? Voluptas, eum dicta? Nemo, ipsam? <br><br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque facere, vero numquam aut consectetur inventore id deleniti recusandae pariatur est? Doloribus tempora, perspiciatis suscipit recusandae velit quas mollitia praesentium aliquam laboriosam debitis numquam similique unde, quibusdam id quae assumenda eos minus eius laudantium blanditiis ipsa dignissimos temporibus aspernatur quis. Nostrum sit corporis molestiae tenetur perferendis aut, iste ut praesentium consequuntur! <br><br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam enim officiis architecto. <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, voluptate, voluptatem aperiam nisi minus eum possimus iusto molestias non nostrum placeat dolores!"
+    date.textContent = "Aug 16th 2010"
+    category.textContent = "World News"
+    author.textContent = "James"
+}
+
+function articleOutdoor(){
+    articleHeading.textContent = "12 Inspirational outdoor living design blogs"
+    articleImg.src = "https://images.unsplash.com/photo-1541959378160-7c0cff4a0966?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    articleImg.alt = "tent in rocks"
+    articleDescription.innerHTML = 
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, excepturi, assumenda perferendis quod quis maxime quos eligendi dolor obcaecati praesentium ex error totam maiores quia, fugiat at nostrum numquam dolorem explicabo velit repellendus mollitia? Beatae earum eum facere ducimus veniam. Sunt deleniti obcaecati provident ut tenetur voluptate libero? Eaque voluptates aliquam natus! Perspiciatis consequatur similique, odio eaque assumenda inventore beatae expedita fuga vero dignissimos est nostrum delectus quae rem qui iure culpa architecto laboriosam! Modi fugit provident natus amet est? <br><br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis velit, tenetur dolore laboriosam magnam optio neque iusto repudiandae totam recusandae. <br><br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, saepe. <br><br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque temporibus nisi non minus quae, iste deserunt reprehenderit soluta consequatur eius, ipsam fugit odio est quod! Quam dolores et maiores quod."
+    date = "Feb 29th 2011"
+    category = "Outdoor Living"
+    author = "Jack"
+}
